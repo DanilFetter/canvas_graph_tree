d898e9e58b93e5ed7d3bcc4279d9dca39cc5372d
Danil Feter <danyafetter.work@gmail.com>
2022-04-26 17:52:56 +0300

    creating commits realised

--- front/src/app/chart-area/chart-area.component.ts
+++ front/src/app/chart-area/chart-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 import * as d3Scale from 'd3';
 import * as d3Shape from 'd3';
@@ -15,7 +15,8 @@ interface commitI{
 @Component({
   selector: 'app-chart-area',
   templateUrl: './chart-area.component.html',
-  styleUrls: ['./chart-area.component.scss']
+  styleUrls: ['./chart-area.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class ChartAreaComponent implements OnInit {
@@ -29,24 +30,46 @@ export class ChartAreaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const svg = d3.select('.graph-start')
-
-    svg.append('rect')
-    .style('width', "100px")
-    .style('height', "20px")
-    .attr('x', '50%')
-    .attr('fill', "#673ab7" )
-    .attr('stroke', "#673ab7" )
-  
-    svg.append("line")
-    .attr("x1",'calc(50% + 50px)')  
-    .attr("y1",20)  
-    .attr("x2",'calc(50% + 50px)')  
-    .attr("y2",70)  
-    .attr("stroke","#673ab7")  
-    .attr("stroke-width",1.3)  
-    .attr("marker-end","url(#arrow)")
+    const lastpoint = d3.select('.graph-start')
+    lastpoint.append('div')
+    .attr('class', "commit-block")
+    .property('textContent', "start")
   }
 
+  addCommit(commitElem: commitI):void{
+    const lastpoint = d3.select('.graph-start')
+
+    lastpoint.append("svg")
+    .attr('width', '100px')
+    .attr('height', '40px')
+
+    .append('line')
+    .attr("x1",'50%')  
+    .attr("y1","0%")  
+    .attr("x2",'50%')  
+    .attr("y2","76%")  
+    .attr("stroke","#673ab7")  
+    .attr("stroke-width",2)  
+    .attr("marker-end","url(#arrow)");  
+    
+
+    lastpoint.append('div')
+    .attr('class', "commit-block")
+    .property('textContent', commitElem.name)
+    this.commitCounter();
+
+  }
+  
+  commitRender(){
+    
+    let activeCommits = this.commitCounter();
+    if( activeCommits < this.commitData.length){
+      this.addCommit(this.commitData[activeCommits]);
+    }
+  }
+
+  commitCounter(){
+      return d3.selectAll(".commit-block").size()-1;
+  }
 
 }
